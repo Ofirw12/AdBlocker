0c1e9ff481b00d48868108518730d2a553f0d16a
ofir.wijsboom <ofir12121@gmail.com>
2025-08-28 10:27:58 +0300

    add blocklist

--- src/DNSBlockList.cpp
+++ src/DNSBlockList.cpp
@@ -1,12 +1,28 @@
-//
-// Created by ofir on 26/08/2025.
-//
+
+#include <fstream>
+
 #include "DNSBlockList.hpp"
 
 bool adblocker::DNSBlockList::Load(const std::string& path)
 {
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        return false;
+    }
+
+    std::string s;
+
+    while (file.good())
+    {
+        std::getline(file, s);
+        m_domains.insert(s);
+    }
+
+    return true;
 }
 
 bool adblocker::DNSBlockList::IsValid(const std::string& domain) const
 {
+    return !m_domains.contains(domain);
 }
