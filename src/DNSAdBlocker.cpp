26b8326a74fde3eee45a57c650a0cecf1cb3f7f3
Ofirw12 <110497857+Ofirw12@users.noreply.github.com>
2025-09-03 16:39:45 +0300

    Socketimple branch done (#2)
    * add functionality to class DNSAdBlocker
    
    * main functionality of adblock class added
    
    * Cmake fixes
    
    ---------
    
    Co-authored-by: Mukelangelo <basovsteam@gmail.com>

--- src/DNSAdBlocker.cpp
+++ src/DNSAdBlocker.cpp
@@ -1,34 +1,94 @@
-//
-// Created by ofir on 26/08/2025.
-//
+
 #include "DNSAdBlocker.hpp"
+
+#include <iostream>
+
+using namespace boost::asio::ip;
 
 adblocker::DNSAdBlocker::DNSAdBlocker(boost::asio::io_context& io,
     const std::string& listen_ip, unsigned short listen_port,
     const std::string& upstream_ip, unsigned short upstream_port,
     const std::string& blocklist_path)
-{
-}
+    : m_listen_socket(io, udp::endpoint(make_address(listen_ip), listen_port)),
+    m_upstream_socket(io, udp::v4()),
+      m_upstream_endpoint(make_address(upstream_ip), upstream_port),
+      m_buffer(), m_blocklist(blocklist_path)
+{}
 
 void adblocker::DNSAdBlocker::Run()
 {
+    m_blocklist.Load();
+    StartReceive();
 }
 
 void adblocker::DNSAdBlocker::StartReceive()
 {
+    auto client = std::make_shared<udp::endpoint>();
+
+    m_listen_socket.async_receive_from(boost::asio::buffer(m_buffer), *client,
+        [this, client](const boost::system::error_code& ec, std::size_t bytes)
+        {
+            if (!ec && bytes > 0)
+            {
+                HandleReceive(bytes, *client);
+            }
+            else
+            {
+                std::cerr << "Receive error: " << ec.message() << std::endl;
+                StartReceive();
+            }
+        });
 }
 
 void adblocker::DNSAdBlocker::HandleReceive(std::size_t bytes_transferred,
-    const boost::asio::ip::udp::endpoint& remote)
+    const udp::endpoint& client)
 {
+    // TODO: parse DNS question properly
+    std::string domain = "sanjagh.com"; // placeholder
+
+    if (m_blocklist.IsValid(domain))
+    {
+        ForwardQuery(m_buffer.data(), bytes_transferred, client);
+    }
+    else
+    {
+        std::cout << "⛔ Blocked: " << domain << std::endl;
+        // TODO: build proper NXDOMAIN
+        // std::vector<uint8_t> fake_response = {/* ... */};
+        std::vector<uint8_t> fake_response = {0x00, 0x01, 0x00, 0x00};
+        SendResponse(fake_response, client);
+    }
 }
 
 void adblocker::DNSAdBlocker::ForwardQuery(const uint8_t* data, size_t len,
-    const boost::asio::ip::udp::endpoint& client)
+                                                const udp::endpoint& client)
 {
+    m_upstream_socket.async_send_to(boost::asio::buffer(data, len), m_upstream_endpoint,
+        [this, client](const boost::system::error_code& ec, std::size_t bytes)
+        {
+            if (!ec)
+            {
+                m_upstream_socket.async_receive_from(boost::asio::buffer(m_buffer), m_upstream_endpoint,
+                    [this, client](const boost::system::error_code& ec, std::size_t bytes)
+                    {
+                        if (!ec)
+                        {
+                            std::vector<uint8_t> response(m_buffer.begin(),
+                              m_buffer.begin() + bytes);
+                            SendResponse(response, client);
+                        }
+                    });
+            }
+        });
 }
 
 void adblocker::DNSAdBlocker::SendResponse(const std::vector<uint8_t>& response,
-    const boost::asio::ip::udp::endpoint& client)
+                                                    const udp::endpoint& client)
 {
+    m_listen_socket.async_send_to(boost::asio::buffer(response), client,
+        [this](const boost::system::error_code& ec, std::size_t bytes)
+        {
+            std::cout << "responded" << std::endl;
+            StartReceive();
+        });
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
+
 #include <iostream>
+
+#include <iomanip>   // for std::setw, std::setfill
+
+#include "DNSAdBlocker.hpp"
+
+using namespace boost::asio::ip;
+
+
+void hex_dump(const uint8_t* data, std::size_t length)
+{
+    for (std::size_t i = 0; i < length; i++)
+    {
+        if (i % 16 == 0) std::cout << std::endl << std::setw(4) << std::setfill('0') << i << ": ";
+        std::cout << std::hex << std::setw(2) << std::setfill('0')
+                  << static_cast<int>(data[i]) << " ";
+    }
+    std::cout << std::dec << std::endl; // back to decimal
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    boost::asio::io_context io;
+    std::string listen_ip = "127.0.0.1";
+    unsigned short listen_port = 5300;
+    std::string dns_ip = "8.8.8.8";
+    unsigned short dns_port = 53;
+    std::string block_list_path = "blocklist.txt";
+    adblocker::DNSAdBlocker adblocker(io, listen_ip, listen_port,
+            dns_ip, dns_port, block_list_path);
+
+    adblocker.Run();
+    io.run();
+
     return 0;
 }
